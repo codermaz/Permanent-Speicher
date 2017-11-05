937d05e51594ff7dfbf6dd768573e5656697d35a
codermaz <yilmazadem@gmail.com>
2017-11-05 16:46:28 +0100

    v2

--- Permanent Speicher/ViewController.swift
+++ Permanent Speicher/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    @IBAction func speichernPressed(_ sender: Any) {
+        UserDefaults.standard.set(textFieldOutlet.text, forKey: "number")
+    }
+    @IBOutlet weak var memoryOutlet: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let numberObject = UserDefaults.standard.object(forKey: "number")
+        if let number = numberObject as? String {
+            memoryOutlet.text = number
+        } else {
+            memoryOutlet.text = ""
+        }
     }
 
     override func didReceiveMemoryWarning() {
